47c04877e684e7401ed70b77e6d5518796c46388
CJ Wong <cj-wong@users.noreply.github.com>
2023-03-16 03:21:25 -0700

    Fix invalid test check

--- main.bash
+++ main.bash
@@ -9,7 +9,7 @@
 script_dir="$(dirname "${BASH_SOURCE[0]}")"
 env="${script_dir}/.venv"
 
-if [[ ! -e "${env}/bin/activate" ]]; then
+if [[ ! -f "${env}/bin/activate" ]]; then
     printf '%s\n' "The virtual environment is missing" >&2
     exit 1
 fi
